bdf020d8991788d63bb7c40c70ed55d1018eb65a
Ian Bishop <ianbishop@pace7.com>
2015-06-04 11:00:33 +1000

    Add interface flag

--- main.go
+++ main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"os"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
+// spanlen should be large enough to capture the layers we're interested in
+const snaplen = 100
+
+var iface = flag.String("i", "eth0", "interface to listen on")
+
 func main() {
+	flag.Parse()
 	var eth layers.Ethernet
 	var ip6 layers.IPv6
 	var icmp layers.ICMPv6
 	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip6, &icmp)
 	decoded := []gopacket.LayerType{}
 
-	if handle, err := pcap.OpenLive("p1p1", 1600, true, 0); err != nil {
-		panic(err)
+	if handle, err := pcap.OpenLive(*iface, snaplen, true, 0); err != nil {
+		fmt.Printf("pcap open error %s\n", err)
+		os.Exit(1)
 	} else {
 		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 		for packet := range packetSource.Packets() {
